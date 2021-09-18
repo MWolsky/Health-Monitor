fb0ac69895f38acbfd6edb721ff2a9854ea0ca03
Michal Wolszczak <michal.wolszczakmw@gmail.com>
2021-09-18 08:59:44 +0200

    Added database.py module with class to work with SQLite database

--- sqlite/database.py
+++ sqlite/database.py
@@ -1,3 +1,52 @@
-import sys
-print(sys.path)
-print(sys.modules)
+import sqlite3 as sql3
+
+class SqliteDataBase:
+    def __init__(self, name: str) -> None:
+        self.con = sql3.connect(f'{name}.db')
+        self.cur = self.cursor()
+
+    def cursor(self) -> sql3.Cursor:
+        """ Creates Cursor required to execute SQL statements
+
+        Returns:
+            sql3.Cursor: database Cursor object
+        """
+        return self.con.cursor()
+
+    def close_connection(self) -> None:
+        """ Closes database connection
+        """
+        self.con.close()
+
+    def commit_changes(self) -> None:
+        """ Commits (saves) changes to database
+        """
+        self.con.commit()
+
+    def execute(self, statement: str, named_params: dict = None) -> None:
+        """Executes given SQL statement
+
+        Args:
+            statement (str): SQL statement provided in text
+        """
+        if named_params:
+            self.cur.execute(statement, named_params)
+        else:
+            self.cur.execute(statement) 
+
+    def execute_many(self, statement: str, parameters: list) -> None:
+        """ Executes given parametrized SQL statement with parameters list
+
+        Args:
+            statement (str): SQL statement provided in text
+            parameters (list): Parameters list to be passed in given order
+        """
+        self.cur.executemany(statement, parameters)
+
+    def execute_batch(self, statements: str) -> None:
+        """ Executes batch of SQL statements
+
+        Args:
+            statements (str): SQL statement provided in text
+        """
+        self.cur.executescript(statements)
